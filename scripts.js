5bb856f6fbce62b6ef9ad262152b2a05e226405e
Linus Fridberg <linus@fridbergs.com>
2023-11-14 18:17:49 +0100

    added script link to js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 // 1 . Anslut till JSON filen  - fetch synch await
 
 async function addCvInfo() {
-  const response = await fetch(cv.json);
+  const response = await fetch("cv.json");
 
   if (response.ok) {
     const cvInfo = await response.json();
-
     //Get the reference to the element inside the HTML document
     const profileName = document.getElementById("profileName");
-
+    const profileSlogan = document.getElementById("profileSlogan");
     //Create the H3 element
     //const profileNameInput = document.createElement("h3");
 
     // Add the content to the H3 element
-    profileName.textContent = cvInfo.fullName;
+    profileName.textContent = cvInfo.name;
+    profileSlogan.textContent = cvInfo.profileText;
   } else {
     console.log(`HTTP error message: ${response.status}`);
   }
